4f9853000c969b38b6c36843bd557da377305ca8
Dmitry Mayorov <voroyam@owncloud.com>
2018-06-22 18:31:48 +0200

    fix

--- rootfs/etc/owncloud.d/95-univention.sh
+++ rootfs/etc/owncloud.d/95-univention.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
 #95-univention.sh
+touch /var/lib/univention-appcenter/apps/owncloud/data/files/owncloud-appcenter.log
 
 to_logfile () {
   tee --append /var/lib/univention-appcenter/apps/owncloud/data/files/owncloud-appcenter.log
 }
 
-
-echo "[95.univeniton.sh] setting up user sync in cron"
-cat << EOF >| /etc/cron.d/sync
-*/10  *  *  *  * root /usr/local/bin/occ user:sync -m disable 'OCA\User_LDAP\User_Proxy'
-EOF
-echo "[95.univeniton.sh] first user sync"
-occ user:sync -m disable "OCA\User_LDAP\User_Proxy" 2>&1 | to_logfile
 
 echo "[95.univeniton.sh] setting collabora URL"
 if [[ "$(occ config:app:get richdocuments wopi_url)" == "" ]]
@@ -24,30 +18,29 @@ OWNCLOUD_PERM_DIR="/var/lib/univention-appcenter/apps/owncloud"
 OWNCLOUD_DATA="${OWNCLOUD_PERM_DIR}/data"
 OWNCLOUD_CONF="${OWNCLOUD_PERM_DIR}/conf"
 
-collabora_log=/var/lib/univention-appcenter/apps/owncloud/data/files/owncloud-appcenter.log
 collabora_cert=/etc/univention/ssl/ucsCA/CAcert.pem
 owncloud_certs=/var/www/owncloud/resources/config/ca-bundle.crt
 
-echo "[95.univeniton.sh] Is the collabora certificate is mounted correctly" >> $collabora_log
+echo "[95.univeniton.sh] Is the collabora certificate is mounted correctly" 
 if [ -f $collabora_cert ]
 then
-          echo "Yes.
-          Was it updated?" >> $collabora_log
-          # Declaring the marker-string
-          collab="This is a certificate for Collabora for ownCloud"
-          if grep -Fq "$collab" "$owncloud_certs"
-          then
-                  echo "Yes. 
-                  Certificate was already updated" >> $collabora_log
-          else
-                  echo "No. 
-                  Updating Certificate..." >>$collabora_log
-                  echo "$collab" >> $owncloud_certs
-                  cat $collabora_cert >> $owncloud_certs
-                  echo "Certificate has been succesfully updated" >> $collabora_log
-          fi
+    echo "Yes.
+    Was it updated?" 
+    # Declaring the marker-string
+    collab="This is a certificate for Collabora for ownCloud"
+    if grep -Fq "$collab" "$owncloud_certs"
+        then
+            echo "Yes. 
+            Certificate was already updated" 
+        else
+            echo "No. 
+            Updating Certificate..." 
+            echo "$collab" >> $owncloud_certs
+            cat $collabora_cert >> $owncloud_certs
+            echo "Certificate has been succesfully updated" 
+    fi
 else 
-          echo "There is no Collabora Certificate" >> $collabora_log        
+    echo "There is no Collabora Certificate"         
 fi
 
 echo "[95.univeniton.sh] configuring owncloud for onlyoffice use"
